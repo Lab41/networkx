ae2a082c20530542c5ab4dacfea64888bd7cf7bc
Lab41PaulM <paulm@lab41.org>
2014-04-28 14:58:23 -0700

    added cesna

--- networkx/algorithms/community/__init__.py
+++ networkx/algorithms/community/__init__.py
@@ -4,3 +4,4 @@ from networkx.algorithms.community.girvan_newman import *
 from networkx.algorithms.community.clauset_newman_moore import *
 from networkx.algorithms.community.bigclam import *
 from networkx.algorithms.community.kclique import *
+from networkx.algorithms.community.cesna import *
